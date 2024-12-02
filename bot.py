68f855db4cf0b837e28e3eac5ac1c914ca188063
totaflix <starastar230@gmail.com>
2024-12-02 10:02:36 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -61,11 +61,11 @@ class Bot(Client):
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
+            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/nowbotupdate for support")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/CodeXBotz")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/nowbotupdate")
         print(ascii_art)
         print("""Welcome to CodeXBotz File Sharing Bot""")
         self.username = usr_bot_me.username
