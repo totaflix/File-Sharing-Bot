182c48614840a89b0c87fa6127124960a81a16db
totaflix <starastar230@gmail.com>
2024-12-02 11:01:39 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,7 +46,7 @@ class Bot(Client):
                 if not link:
                     await self.export_chat_invite_link(FORCE_SUB_CHANNEL)
                     link = (await self.get_chat(FORCE_SUB_CHANNEL)).invite_link
-                self.invitelink = https://t.me/nowbotupdate
+                self.invitelink = link
             except Exception as a:
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
