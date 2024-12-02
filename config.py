774117f2aecff996304d992cc542f12a785641cf
totaflix <starastar230@gmail.com>
2024-12-02 11:31:14 +0600

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ API_HASH = os.environ.get("API_HASH", "31cb93c017f265e4fa6d0ba91236b826")
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002497075001"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "5659668981")
+OWNER_ID = int(os.environ.get("OWNER_ID","5659668981")
 
 #Port
 PORT = os.environ.get("PORT", "8080")
